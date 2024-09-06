e6372f0ec5651abc0e8eb47a63373daa07bd8d08
Bottle-Water <78448678+Bottle-Water@users.noreply.github.com>
2024-09-06 17:30:58 -0400

    database connection fix

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -5,7 +5,7 @@
 	$searchResults = "";
 	$searchCount = 0;
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "contact_manager");
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
