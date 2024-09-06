3ff475896ff02cfd902ac205b8279820b85a168e
Bottle-Water <78448678+Bottle-Water@users.noreply.github.com>
2024-09-06 17:43:09 -0400

    delete contact init

---
+++ LAMPAPI/DeleteContact.php
@@ -0,0 +1,47 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "contact_manager");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+    {
+        $contactId = $inData["contactId"];
+        $stmt = $conn->prepare("DELETE FROM Contacts WHERE ID = ?");
+        $stmt->bind_param("i", $contactId);
+        $stmt->execute();
+        $stmt->close();
+
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+        $retValue = '{"id":0,"name":"","email":"","phone":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
