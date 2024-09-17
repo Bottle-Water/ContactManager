4cbaff17639f85b57c1ebab3cb868b48fecac572
Sean Previty <seanprevity@gmail.com>
2024-09-17 16:41:02 -0400

    updated add

--- frontend/src/components/Add_Contact.js
+++ frontend/src/components/Add_Contact.js
@@ -9,7 +9,6 @@ const Add_Contact = (props) => {
 
     const add = async (e) => {
         e.preventDefault();
-
         const userID = localStorage.getItem('userID');
 
         const url = 'http://gerberknights3.xyz/LAMPAPI/addContact.php';
@@ -23,17 +22,10 @@ const Add_Contact = (props) => {
                 body: JSON.stringify({ name, email, phone, userID })
             });
             const data = await response.json();
-            const id = data.ID;
-            props.Add_Contact_Handler({ name, email, phone, userID , id});
             console.log("Response data:", data);
         } catch (error) {
             console.error('Error adding contact:', error);
         }
-
-        setName("");
-        setEmail("");
-        setPhone("");
-
         navigate("/contact_list"); 
     };
 
